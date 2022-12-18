9f92f88209b2a990c2f194e6bd275080616e8959
Serhey <serhey781@gmail.com>
2022-12-18 22:05:54 +0200

    version.1.0.0.0

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    app: "./src/assets/js/index.js",
+  },
+
+  output: {
+    clean: true,
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  mode: "development",
+  devServer: {
+    static: "./src",
+    compress: true,
+    port: 9000,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s[as]ss|css)$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Portfolio",
+      template: "src/index.html",
+    }),
+  ],
+};
